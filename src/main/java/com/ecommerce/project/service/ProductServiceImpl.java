24f77ede574f6fb3b8deddeb50951b5dfd857f31
DeeprajGawand <deeprajgawand@gmail.com>
2024-10-13 19:10:55 +0530

    created new file in service layer for update image

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -9,6 +9,7 @@ import com.ecommerce.project.repositories.CategoryRepository;
 import com.ecommerce.project.repositories.ProductRespository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -30,6 +31,12 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ModelMapper modelMapper;
+
+    @Autowired
+    private FileService fileService;
+
+    @Value("${project.image}")
+    private String path;
 
     @Override
     public ProductDTO addProduct(Long categoryId, ProductDTO productDTO) {
@@ -117,8 +124,7 @@ public class ProductServiceImpl implements ProductService {
                 .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
         //upload image (to server)
         //get the file name of uploaded image
-        String path = "images/";
-        String fileName = uploadImage(path, image);
+        String fileName = fileService.uploadImage(path, image);
 
         //updating the new file name to the product
         productFromDb.setImage(fileName);
@@ -131,24 +137,6 @@ public class ProductServiceImpl implements ProductService {
         return modelMapper.map(updatedProduct, ProductDTO.class) ;
     }
 
-    private String uploadImage(String path, MultipartFile file) throws IOException {
-        // File names of current/ original file
-        String originalFileName = file.getOriginalFilename();
-
-        //generate a uniques file name
-        String randomId = UUID.randomUUID().toString();
-        //mat.jpg --> 1234 ---> 1234.jpg
-        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
-        String filePath = path + File.separator + fileName;
-        //check if path exists and create
-        File folder = new File(path);
-        if (!folder.exists())
-            folder.mkdir();
-        //upload to server
-        Files.copy(file.getInputStream(), Paths.get(filePath));
-        return fileName;
-        //return file name
-    }
 
 
 }
