7c69ad76a3b2f2562e02276d0c5ad5f76b7c049c
DeeprajGawand <deeprajgawand@gmail.com>
2024-10-13 18:58:02 +0530

    update product image api completed

--- src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -10,8 +10,14 @@ import com.ecommerce.project.repositories.ProductRespository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ProductServiceImpl implements ProductService {
@@ -32,7 +38,7 @@ public class ProductServiceImpl implements ProductService {
                 ResourceNotFoundException("Category", "categoryId", categoryId));
 
         Product product = modelMapper.map(productDTO, Product.class);
-        product.setImage("DefaultPng");
+        product.setImage("default.png");
         product.setCategory(category);
         double specialPrice = product.getPrice() - (product.getDiscount() * 0.01 * product.getPrice());
         product.setSpecialPrice(specialPrice);
@@ -102,6 +108,46 @@ public class ProductServiceImpl implements ProductService {
         productRespository.delete(productFromDb);
         return modelMapper.map(productFromDb, ProductDTO.class);
 
+    }
+
+    @Override
+    public ProductDTO updateProductImage(Long productId, MultipartFile image) throws IOException {
+        //get the product from the db
+        Product productFromDb = productRespository.findById(productId)
+                .orElseThrow(() -> new ResourceNotFoundException("Product", "productId", productId));
+        //upload image (to server)
+        //get the file name of uploaded image
+        String path = "images/";
+        String fileName = uploadImage(path, image);
+
+        //updating the new file name to the product
+        productFromDb.setImage(fileName);
+
+        //Save Updated product
+        Product updatedProduct = productRespository.save(productFromDb);
+
+        // return DTO after mapping product to DTO
+
+        return modelMapper.map(updatedProduct, ProductDTO.class) ;
+    }
+
+    private String uploadImage(String path, MultipartFile file) throws IOException {
+        // File names of current/ original file
+        String originalFileName = file.getOriginalFilename();
+
+        //generate a uniques file name
+        String randomId = UUID.randomUUID().toString();
+        //mat.jpg --> 1234 ---> 1234.jpg
+        String fileName = randomId.concat(originalFileName.substring(originalFileName.lastIndexOf('.')));
+        String filePath = path + File.separator + fileName;
+        //check if path exists and create
+        File folder = new File(path);
+        if (!folder.exists())
+            folder.mkdir();
+        //upload to server
+        Files.copy(file.getInputStream(), Paths.get(filePath));
+        return fileName;
+        //return file name
     }
 
 
