a209d9771b0c3be2b7fd7613f5dc60fa6c13b0e3
DeeprajGawand <deeprajgawand@gmail.com>
2024-10-22 12:09:05 +0530

    created user,address,role entity with app role enum

---
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -0,0 +1,17 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "addresses")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long addressId;
+}
---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,43 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "username")
+    private String userName;
+
+    @NotBlank
+    @Size(max = 50)
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    @Column(name = "password")
+    private String password;
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+}
