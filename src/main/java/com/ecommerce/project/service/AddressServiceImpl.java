fa991274601c35c8dd83de3ae73e8af757783986
DeeprajGawand <deeprajgawand@gmail.com>
2024-11-07 12:28:19 +0530

    create address api created

---
+++ src/main/java/com/ecommerce/project/service/AddressServiceImpl.java
@@ -0,0 +1,48 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Address;
+import com.ecommerce.project.model.User;
+import com.ecommerce.project.payload.AddressDTO;
+import com.ecommerce.project.repositories.AddressRepository;
+import com.ecommerce.project.repositories.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+
+    @Autowired
+    private AddressRepository addressRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public AddressDTO createAddress(AddressDTO addressDTO, User user) {
+        //mapping the address dto to address class and saving to address object
+        Address address = modelMapper.map(addressDTO, Address.class);
+
+        //Associating the User with the Address:
+        address.setUser(user);
+
+        //  Updating the User's Address List:You retrieve the existing list of addresses from the user.
+        //  Then, add the new address to this list.
+        //Finally, update the user's address list with this new list containing the added address.
+        // This is essential if the list of addresses is managed within the User entity,
+        // ensuring both sides of the relationship (the user and address) are properly updated.
+        List<Address> addressesList = user.getAddresses();
+        addressesList.add(address);
+        user.setAddresses(addressesList);
+
+        //Saving the Address:This saves the Address entity to the database.
+        Address savedAddress = addressRepository.save(address);
+
+        return modelMapper.map(savedAddress, AddressDTO.class);
+    }
+}
